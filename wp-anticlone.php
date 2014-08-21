cf565f3aa626cb6cee0b320789b3d9bcf1226865
Sebastian Bauer <sebastian.bauer@inovex.de>
2014-08-21 15:24:46 +0200

    Added plugin code

--- wp-anticlone.php
+++ wp-anticlone.php
@@ -23,3 +23,112 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+add_filter( 'wp_head', 'wpac_add_js' );
+
+function wpac_add_js($content) {
+  $domains = explode(',' str_replace(' ', '', get_option('wp-anticlone_settings')));
+  $content .= <<<EOD
+    <script type="text/javascript">
+      var wpac_domains = [
+    EOD;
+  foreach($domains as $domain){
+    $content .= '\''.base64_encode(str_replace('.www', '', $domain)).'\',';
+  }
+  $content .= <<<EOD
+      ];
+      var wpac_hasMatches = false;
+      var wpac_loc = btoa(window.location.host.replace('www.', ''));
+      for(var i in wpac_domains) {
+        if(wpac_domains[i] == wpac_loc) {
+          wpac_hasMatches = true;
+        }
+      }
+      window.onload = function() {
+        if(wpac_hasMatches == false) {
+          var wpac_newlocation = window.location.href.replace(window.location.host, atob(wpac_domains[0]));
+          document.body.innerHTML = '<span style="font-size: 16px;color: red;font-weight: bold;">Diese Seite wurde unrechtm&auml;&szlig;ig geklont. Die echte Seite finden Sie unter '+wpac_newlocation+'. Weitere Information zu diesem illegalen Vorgehen finden Sie beispielsweise hier: <a href="http://niedblog.de/blog-kopiert-name-geklaut/" target="_blank">niedblog.de</a></span>';
+          window.location = wpac_newlocation;
+        }
+      }
+    </script>
+  EOD;
+}
+add_action( 'admin_menu', 'wpac_add_admin_menu' );
+add_action( 'admin_init', 'wpac_settings_init' );
+
+
+function wpac_add_admin_menu(  ) {
+
+	add_options_page( 'WordPress Anti-Clone', 'WordPress Anti-Clone', 'manage_options', 'wp-anticlone', 'wp-anticlone_options_page' );
+
+}
+
+
+function wpac_settings_exist(  ) {
+
+	if( false == get_option( 'wp-anticlone_settings' ) ) {
+
+		add_option( 'wp-anticlone_settings' );
+
+	}
+
+}
+
+
+function wpac_settings_init(  ) {
+
+	register_setting( 'pluginPage', 'wpac_settings' );
+
+	add_settings_section(
+		'wpac_pluginPage_section',
+		__( 'Your section description', 'wordpress' ),
+		'wpac_settings_section_callback',
+		'pluginPage'
+	);
+
+	add_settings_field(
+		'wpac_text_field_0',
+		__( 'Liste von authorisierten Domains, kommasepariert (z.B. foobar.de,foobar.info )', 'wordpress' ),
+		'wpac_text_field_0_render',
+		'pluginPage',
+		'wpac_pluginPage_section'
+	);
+
+
+}
+
+
+function wpac_text_field_0_render(  ) {
+
+	$options = get_option( 'wpac_settings' );
+	?>
+	<input type='text' name='wpac_settings[wpac_text_field_0]' value='<?php echo $options['wpac_text_field_0']; ?>'>
+	<?php
+
+}
+
+
+function wpac_settings_section_callback(  ) {
+
+	echo __( 'This section description', 'wordpress' );
+
+}
+
+
+function wp-anticlone_options_page(  ) {
+
+	?>
+	<form action='options.php' method='post'>
+
+		<h2>wp-anticlone</h2>
+
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+
+	</form>
+	<?php
+
+}
