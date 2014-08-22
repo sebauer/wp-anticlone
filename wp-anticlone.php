0e3eaa21b1d05cddab4e5b2fe75390d970c755c1
Sebastian Bauer <sebastian.bauer@inovex.de>
2014-08-22 12:22:12 +0200

    Updated version string

--- wp-anticlone.php
+++ wp-anticlone.php
@@ -3,7 +3,7 @@
  * Plugin Name: WordPress Anti-Clone
  * Plugin URI: https://github.com/sebauer/wp-anticlone
  * Description: A WordPress plugin to reduce the impact of unauthorized clones of your blog.
- * Version: 0.1.1
+ * Version: 0.1.2
  * Author: Sebastian Bauer
  * Author URI: https://github.com/sebauer
  * License: GPL2
