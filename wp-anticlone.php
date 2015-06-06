1b921ceb583c245f18e06db56b845251771a285d
Karsten Gohm <kasn@mac.com>
2015-06-06 19:38:01 +0200

    track bad referrers, list referrer in backend, make code less predictable, make message customizeable

--- wp-anticlone.php
+++ wp-anticlone.php
@@ -23,8 +23,43 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
+global $wpac_version;
+$wpac_db_version = '1.0';
+
+function wpac_update_db_check() {
+	global $wpac_db_version;
+
+	if ( get_site_option( 'wpac_db_version' ) != $wpac_db_version ) {
+		wpac_install();
+	}
+}
+
+function wpac_install() {
+	global $wpdb;
+	global $wpac_db_version ;
+
+
+	$table_name = $wpdb->prefix . 'wpac_referrer';
+
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id int NOT NULL AUTO_INCREMENT,
+		time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+		referrer VARCHAR (255) NOT NULL,
+		PRIMARY KEY id (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'wpac_db_version', $wpac_db_version );
+	add_option( 'wpac_random_param', uniqid() );
+}
+
+add_action( 'plugins_loaded', 'wpac_update_db_check' );
 add_action( 'wp_footer', 'wpac_add_js' );
-//add_action( 'wp_head', 'wpac_add_fakecss' );
 
 function wpac_add_fakecss() {
   $pluginUrl = plugins_url();
@@ -32,29 +67,54 @@ function wpac_add_fakecss() {
 }
 
 function wpac_add_js() {
-  $domains = get_option('wpac_settings');
-  $domains = $domains['wpac_authDomains'];
-  if($domains == '') return;
+  $options = get_option('wpac_settings');
+  $domains = $options['wpac_authDomains'];
+  $message = $options['wpac_message'];
+  if($domains == '') {
+	  return;
+  }
   $domains = explode(',', str_replace(' ', '', $domains));
-  $pluginsUrl = plugins_url();
   $domainString = '';
   foreach($domains as $domain){
     $domainString .= '\''.base64_encode(str_replace('.www', '', $domain)).'\',';
   }
 
+	$span_start = '<span style="font-size: 16px;color: red;font-weight: bold;">';
+	$span_end = '</span>';
+
+	if (!message != '') {
+		$message_start = base64_encode($span_start . 'Diese Seite wurde unrechtm&auml;&szlig;ig geklont. Die echte Seite finden Sie unter');
+		$message_end = base64_decode($span_end);
+	} else {
+		if ($replace_position = strpos( $message, '%page%' ) ) {
+			$message_start = base64_encode($span_start . substr($message, 0, $replace_position));
+			$message_end = base64_encode(substr($message, $replace_position + 6) . $span_end);
+		} else {
+			$message_start = base64_encode( '<span style="font-size: 16px;color: red;font-weight: bold;">' . $message );
+			$message_end = base64_decode($span_end);
+		}
+	}
+
+	$niedblog = '<!-- -->';
+	if ($options['wpac_niedblog'] == 1) {
+		$niedblog = '<br/>' . $span_start . 'Weitere Information zu diesem illegalen Vorgehen finden Sie beispielsweise hier: <a href="http://niedblog.de/blog-kopiert-name-geklaut/" rel="nofollow" target="_blank">niedblog.de</a>' . $span_end;
+	}
+	$niedblog =  base64_encode($niedblog);
+
+  $newlocation = uniqid('_');
   $has_matches = uniqid('_');
   $domains = uniqid('_');
   $loc = uniqid('_');
-  $newlocation = uniqid('_');
+  $random_param = get_option('wpac_random_param', 'wpac_s');
   $content = <<<EOD
-  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=" style="display: none;" onload="window.{$domains}=[{$domainString}];var {$has_matches}=false;var {$loc}=btoa(window.location.host.replace('www.',''));var {$domains}=window.{$domains};for(var i in {$domains}){if({$domains}[i]=={$loc}){{$has_matches}=true;}}if({$has_matches}==false){var {$newlocation}=window.location.href.replace(window.location.host,atob({$domains}[0]));document.body.innerHTML=atob('PHNwYW4gc3R5bGU9ImZvbnQtc2l6ZTogMTZweDtjb2xvcjogcmVkO2ZvbnQtd2VpZ2h0OiBib2xkOyI+RGllc2UgU2VpdGUgd3VyZGUgdW5yZWNodG0mYXVtbDsmc3psaWc7aWcgZ2VrbG9udC4gRGllIGVjaHRlIFNlaXRlIGZpbmRlbiBTaWUgdW50ZXIg')+{$newlocation}+atob('LiBXZWl0ZXJlIEluZm9ybWF0aW9uIHp1IGRpZXNlbSBpbGxlZ2FsZW4gVm9yZ2VoZW4gZmluZGVuIFNpZSBiZWlzcGllbHN3ZWlzZSBoaWVyOiA8YSBocmVmPSJodHRwOi8vbmllZGJsb2cuZGUvYmxvZy1rb3BpZXJ0LW5hbWUtZ2VrbGF1dC8iIHJlbD0ibm9mb2xsb3ciIHRhcmdldD0iX2JsYW5rIj5uaWVkYmxvZy5kZTwvYT48L3NwYW4+');{$newlocation}+='?wpac_s='+window.location.host;window.location={$newlocation}}" />
+  <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=" style="display: none;" onload="window.{$domains}=[{$domainString}];var {$has_matches}=false;var {$loc}=btoa(window.location.host.replace('www.',''));var {$domains}=window.{$domains};for(var i in {$domains}){if({$domains}[i]=={$loc}){{$has_matches}=true;}}if({$has_matches}==false){var {$newlocation}=window.location.href.replace(window.location.host,atob({$domains}[0]));document.body.innerHTML=atob('{$message_start}')+{$newlocation}+atob('{$message_end}')+atob('{$niedblog}');{$newlocation}+='?{$random_param}='+window.location.host;window.location={$newlocation}}" />
 EOD;
 
   echo $content;
 }
 add_action( 'admin_menu', 'wpac_add_admin_menu' );
 add_action( 'admin_init', 'wpac_settings_init' );
-
+add_action( 'init', 'wp_anticlone_frontend_init');
 
 function wpac_add_admin_menu(  ) {
 
@@ -68,6 +128,13 @@ function wpac_settings_exist(  ) {
 	if( false == get_option( 'wp-anticlone_settings' ) ) {
 
 		add_option( 'wp-anticlone_settings' );
+		update_option(
+			'wp-anticlone_settings',
+			array(
+				'wpac_message' => 'Diese Seite wurde unrechtm&auml;&szlig;ig geklont. Die echte Seite finden Sie unter',
+				'niedblog'     => 1,
+			)
+		);
 
 	}
 
@@ -93,6 +160,28 @@ function wpac_settings_init(  ) {
 		'wpac_pluginPage_section'
 	);
 
+	add_settings_field(
+		'wpac_message',
+		__( 'Nachricht die dem Besuchen angezeigt werden soll', 'wordpress' ),
+		'wpac_text_field_1_render',
+		'pluginPage',
+		'wpac_pluginPage_section'
+	);
+
+	add_settings_field(
+		'wpac_niedblog',
+		__( 'Soll ein Link zum Niedblog Artikel gezeigt werden', 'wordpress' ),
+		'wpac_checkbox_field_0_render',
+		'pluginPage',
+		'wpac_pluginPage_section'
+	);
+
+	add_settings_section(
+		'wpac_pluginPage_stats',
+		__( 'Statistiken', 'wordpress' ),
+		'wpac_settings_section_stats',
+		'pluginPage'
+	);
 
 }
 
@@ -105,9 +194,42 @@ function wpac_text_field_0_render(  ) {
 }
 
 
+function wpac_text_field_1_render(  ) {
+
+	$options = get_option( 'wpac_settings' );
+	echo "<input type='text' name='wpac_settings[wpac_message]' value='".$options['wpac_message']."'><small><strong>%page%<strong> wird durch die originale Seite ersetzt!";
+
+}
+
+function wpac_checkbox_field_0_render() {
+	$options = get_option( 'wpac_settings' );
+	echo "<input type='checkbox' name='wpac_settings[wpac_niedblog]' value='1'";
+	if ($options['wpac_niedblog'] == 1) {
+		echo 'checked="checked"';
+	}
+
+	echo "/>";
+}
+
+
 function wpac_settings_section_callback(  ) {
 
 	echo __( '', 'wordpress' );
+
+}
+
+function wpac_settings_section_stats() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'wpac_referrer';
+
+	$referrer = $wpdb->get_results('SELECT referrer, count(referrer) as c from ' . $table_name . ' GROUP BY referrer ORDER BY c DESC');
+
+	echo '<ul>';
+	foreach ($referrer as $domain) {
+		echo '<li><strong>' . $domain->referrer . '</strong> (' . $domain->c . ')</li>';
+	}
+	echo '</ul>';
 
 }
 
@@ -128,4 +250,22 @@ function wp_anticlone_options_page(  ) {
 	</form>
 	<?php
 
+}
+
+
+
+function wp_anticlone_frontend_init()
+{
+	$random = get_option( 'wpac_random_param', false);
+
+	if (isset($_GET[$random])) {
+		global $wpdb;
+
+		$wpdb->insert(
+			$wpdb->prefix . 'wpac_referrer',
+			array(
+				'referrer' => $_GET[$random],
+			)
+		);
+	}
 }
